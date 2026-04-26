18ba9ea0657157fa76965a2bded7a1468f0e9673
sairam-1023 <sairamamaraneni22@gmail.com>
2026-04-26 15:39:53 +0530

    Updated entire project

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 import pickle
+import models
+from auth import hash_password, verify_password, create_token
 import numpy as np
+from database import SessionLocal, engine
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt
+
+
+models.Base.metadata.create_all(bind=engine)
+
 
 class InputData(BaseModel):
     sqft_living: float
@@ -11,6 +20,29 @@ class InputData(BaseModel):
     bathrooms: float
 
 app = FastAPI()
+
+security = HTTPBearer()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, "secret123", algorithms=["HS256"])
+        return payload["sub"]
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+# DB dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
 
 # Allow frontend access
 app.add_middleware(
@@ -20,10 +52,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Input schema
-class InputData(BaseModel):
-    area: float
-    bedrooms: int
+\
 
 # Load model
 with open("model.pkl", "rb") as f:
@@ -34,7 +63,34 @@ def home():
     return {"message": "Backend running"}
 
 @app.post("/predict")
-def predict(data: InputData):
+def predict(data: InputData, user: str = Depends(get_current_user)):
+    # only logged-in users can access
     input_data = [[data.sqft_living, data.bedrooms, data.bathrooms]]
     prediction = model.predict(input_data)[0]
-    return {"predicted_price": float(prediction)}
+    return {"user": user, "predicted_price": float(prediction)}
+
+
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    existing = db.query(models.User).filter(models.User.username == user.username).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    new_user = models.User(
+        username=user.username,
+        password=hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+
+    return {"message": "User created"}
+
+@app.post("/login")
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+
+    if not db_user or not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_token({"sub": user.username})
+    return {"access_token": token}
